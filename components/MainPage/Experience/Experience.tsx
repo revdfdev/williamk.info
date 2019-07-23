961d0863916e0289a12660926d1bf7fb6b010a86
William Kwok <wkwok16@uw.edu>
2019-07-22 23:57:47 -0700

    Added experience information

--- components/MainPage/Experience/Experience.tsx
+++ components/MainPage/Experience/Experience.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import QualtricsLogo from '../../_assets/Images/Experience/QualtricsLogo.png';
 import IugaLogo from '../../_assets/Images/Experience/IugaLogo.png';
+import iGEMLogo from '../../_assets/Images/Experience/WashingtonIGEMLogo.png';
+import iSchoolLogo from '../../_assets/Images/Experience/iSchoolLogo.png';
 
 type ExperienceLink = {
     link: string,
-    title: string
+    title: string,
+    internal?: boolean
 }
 
 type Experience = {
@@ -52,7 +55,83 @@ const EXPERIENCES: Experience[] = [
                 title: "IUGA Homepage"
             }
         ]
+    },
+    {
+        logo: iGEMLogo,
+        company: "Washington iGEM",
+        location: "Seattle, Washington",
+        title: "Lead Front End Engineer",
+        startDate: "February 2017",
+        description: [
+            "I am in charge of development of the team website (WIP) and iGEM Team Wiki Site (required for competition judging). The team website will use NextJS to produce static files hosted on github pages. The Team Wikis use React and TypeScript under the hood. We have implemented a content management system into the Team Wiki so anyone on our team can edit the website."
+        ],
+        links: [
+            {
+                link: "http://uwigem.com",
+                title: "Washington iGEM Website"
+            },
+            {
+                link: "https://github.com/uwigem/wiki2019",
+                title: "2019 Wiki Code"
+            },
+            {
+                link: "https://2019.igem.org/Team:Washington",
+                title: "2019 Wiki"
+            }
+        ]
+    },
+    {
+        logo: iSchoolLogo,
+        company: "University of Washington Information School",
+        location: "Seattle, Washington",
+        title: "Teaching Assistant/Tutor (INFO 340, INFO 441)",
+        startDate: "September 2018",
+        description: [
+            "I was the Teaching Assistant for INFO 340, Client Side Development. This course covered the skills and techniques necessary for creating effective, dynamic, and responsive web applications that meet user needs. This course covered HTML, CSS, JavaScript, jQuery, React, Firebase, and React Router. During lab sections, I went in depth on higher order functions, destructuring and spread operations, jQuery vs React, async await vs .then, firebase security, and testing with Jest and Enzyme.",
+
+            "For tutoring, I was a resource for students enrolled in technical informatics classes. I helped and tutored students on all technical content in required courses, such as INFO 201 Technical Foundations of Informatics, INFO 340 Client Side Development, and INFO 330 Databases and Data Modeling. I provided assistance with R, R Shiny, R Markdown, git, HTML, CSS, JavaScript, jQuery, React, Firebase, React Router, misc JavaScript libraries, and SQL.",
+
+            "In addition, I have created a tutor hub and tutor queue. The tutor hub is a page hosted on my website with written tutorials on how to do various topics covered within the courses, as well as video tutorials for some of the topics. I collected data on the traffic on these pages to determine what topics people struggle with the most. The tutor queue is a queuing system for first come first serve questions by students when me or other tutor(s) are in the lab space. This will also collect usage data for hard topics.",
+
+            "I am also the Teaching Assistant for INFO 441, Server Side Development. One version of this course covers the basics of Django, the Django Rest Framework, deploying on Microsoft Azure, and how it fits together in the context of web development. We also cover bonus topics such as Websockets using NodeJS and security tips. Me and the other TA are in charge of creating lab section slides, creating the weekly assignments, the automated graders for those assignments, and grading the assignments themselves.",
+
+            "Another version of the course covers Golang, Docker, Microservice Architecture, Message Queues, Websockets, and implementing Authentication from scratch."
+        ]
+    },
+    {
+        logo: iSchoolLogo,
+        company: "Code and Cognition Lab - University of Washington Information School",
+        location: "Seattle, Washington",
+        title: "Research Intern",
+        startDate: "June 2018",
+        endDate: "June 2019",
+        description: [
+            "At the Code and Cognition Lab led by Dr. Andy Ko, I design and build research prototypes using ReactJS, plan studies, gather data, and prepare presentations.",
+
+            "I helped code a web tutor that uses a Bayesian Knowledge Tracing algorithm to help users learn programming concepts. I established the framework for writing unit tests in the application, and helped remove code redundancy. I helped plan the pilot study.",
+
+            "In addition, I am an author on a paper: Teaching Explicit Programming Strategies to Adolescents. It investigates the teaching of explicit strategies in the classroom to adolescent students. This paper was accepted to SIGCSE 2019. The primary investigators of the paper were Dr. Thomas LaToza from George Mason University and Dr. Andy Ko from University of Washington."
+        ],
+        links: [
+            {
+                link: "http://faculty.washington.edu/ajko/students",
+                title: "Lab Website"
+            },
+            {
+                link: "https://medium.com/bits-and-behavior",
+                title: "Bits and Behavior - Lab Blog"
+            },
+            {
+                link: "http://faculty.washington.edu/ajko/papers/Ko2019TeachingExplicitStrategies.pdf",
+                title: "Teaching Explicit Programming Strategies to Adolescents"
+            },
+            {
+                link: "https://codeitz.com/",
+                title: "CodeItz: An Intelligent Tutoring System"
+            }
+        ]
     }
+
 ]
 
 export const Experience: React.FC = () => {
