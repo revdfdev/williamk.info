b6fec24fdeb336a21d3a629f08c44ee22d4e03b5
William Kwok <wkwok16@uw.edu>
2019-07-31 00:22:19 -0700

    Added some resume stuff

--- components/Resume/Resume.tsx
+++ components/Resume/Resume.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { PDFExport } from '@progress/kendo-react-pdf';
+import { ResumePage } from './ResumePage/ResumePage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Button } from 'reactstrap';
+import './Resume.css';
 
 export const Resume: React.FC = () => {
+    const pdf = useRef(null);
+    const devCanvas = useRef(null);
+    // TODO: Create a context for devCanvas and svgToImage function that takes devCanvas as parameter
     return <>
+        <canvas ref={devCanvas} className="resume-dev-canvas"></canvas>
+        <Button onClick={() => {
+            // @ts-ignore
+            pdf.current.save()
+        }}>Download</Button>
         <PDFExport paperSize={'Letter'}
             fileName="WilliamKwokResume.pdf"
             title="William Kwok"
             subject="William Kwok"
-
+            ref={pdf}
         >
-            <div style={
-                {
-                    height: 792,
-                    width: 612,
-                    padding: 'none',
-                    backgroundColor: 'white',
-                    boxShadow: '5px 5px 5px #888888',
-                    margin: 'auto',
-                    overflowX: 'hidden',
-                    overflowY: 'hidden'
-                }
-            }>
-                test
-            </div>
+            <ResumePage>
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </ResumePage>
         </PDFExport>
     </>
 }
---
+++ components/Resume/ResumePage/ResumePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ResumePage: React.FC = ({ children }) => {
+    return <div className="resume-page">
+        {children}
+    </div>
+}
